a40ca5bf39c1f3c0a5d2f4ec529fc1e55a86bf7e
Izan Buil <bzancio@gmail.com>
2023-11-27 21:20:44 +0100

    caracteres piezas

--- config.py
+++ config.py
@@ -1,2 +1,28 @@
 # Indica el número del turno actual
 num_turno = 1
+
+
+#Incica como se representaran las diferentes piezas
+PEON_BLANCO = "P"
+
+PEON_NEGRO = "p"
+
+TORRE_BLANCA = "T"
+
+TORRE_NEGRA = "t"
+
+CABALLO_BLANCO = "C"
+
+CABALLO_NEGRO = "c"
+
+ALFIL_BLANCO = "A"
+
+ALFIL_NEGRO = "a"
+
+DAMA_BLANCA = "D"
+
+DAMA_NEGRA = "d"
+
+REY_BLANCO = "R"
+
+REY_NEGRO = "r"
